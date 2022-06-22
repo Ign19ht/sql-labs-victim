90c209a315a58be1809599f5e5bb777d85de5f54
Ignat <petrov-ignat85@mail.ru>
2022-06-22 19:05:12 +0300

    added mariadb

--- main.py
+++ main.py
@@ -5,15 +5,40 @@ import uvicorn
 from fastapi import FastAPI, Request, Form, Cookie
 from fastapi.responses import FileResponse, HTMLResponse
 import psycopg2
+import mariadb
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from enum import Enum
+import sys
 
 
-def db_request(query: str):
+class DataBaseType(Enum):
+    POSTGRES = 1
+    MARIA = 2
+
+
+db_type = None
+
+
+def db_request_postgres(query: str):
     con = psycopg2.connect(database="sql-labs", user="postgres",
                            password="qwerlodaza", host="127.0.0.1", port="5432")
     cur = con.cursor()
     cur.execute(query)
+    rows = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return rows
+
+
+def db_request_maria(query: str):
+    con = mariadb.connect(database="test", user="root",
+                           password="qwerlodaza", host="127.0.0.1", port=3306)
+    cur = con.cursor()
+    cur.execute(query)
+    # print(cur)
     rows = cur.fetchall()
 
     cur.close()
@@ -37,13 +62,21 @@ def get_news(rows):
 
 def check_cookie(session):
     cookie_query = f"SELECT * FROM cookies WHERE cookie='{session}'"
-    cookie_rows = db_request(cookie_query)
+    cookie_rows = None
+    if db_type == DataBaseType.MARIA:
+        cookie_rows = db_request_maria(cookie_query)
+    else:
+        cookie_rows = db_request_postgres(cookie_query)
 
 
 def get_new_cookie() -> str:
     cookie_id = random.randint(0, 19)
     cookie_query = f"SELECT cookie FROM cookies WHERE id='{cookie_id}'"
-    cookie_rows = db_request(cookie_query)
+    cookie_rows = None
+    if db_type == DataBaseType.MARIA:
+        cookie_rows = db_request_maria(cookie_query)
+    else:
+        cookie_rows = db_request_postgres(cookie_query)
     return cookie_rows[0][0]
 
 
@@ -56,7 +89,11 @@ templates = Jinja2Templates(directory="templates/victim")
 async def use_filter(request: Request, category: str, session: str = Cookie("")):
     check_cookie(session)
     query = f"SELECT title, body, image_name FROM news WHERE category='{category}' AND hidden=0"
-    rows = db_request(query)
+    rows = None
+    if db_type == DataBaseType.MARIA:
+        rows = db_request_maria(query)
+    else:
+        rows = db_request_postgres(query)
     print(len(rows))
     response = templates.TemplateResponse("index.html",
                                           {"request": request, "news_rows": get_news(rows), "category": category})
@@ -69,7 +106,11 @@ async def use_filter(request: Request, category: str, session: str = Cookie(""))
 async def show_all(request: Request, session: str = Cookie("")):
     check_cookie(session)
     query = f"SELECT title, body, image_name FROM news WHERE hidden=0"
-    rows = db_request(query)
+    rows = None
+    if db_type == DataBaseType.MARIA:
+        rows = db_request_maria(query)
+    else:
+        rows = db_request_postgres(query)
     response = templates.TemplateResponse("index.html",
                                           {"request": request, "news_rows": get_news(rows), "category": "All"})
     if not session:
@@ -93,7 +134,11 @@ async def open_login(request: Request, session: str = Cookie("")):
 @app.post("/auth")
 async def login(request: Request, username: str = Form(), password: str = Form()):
     query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
-    rows = db_request(query)
+    rows = None
+    if db_type == DataBaseType.MARIA:
+        rows = db_request_maria(query)
+    else:
+        rows = db_request_postgres(query)
     if len(rows) > 0:
         return 1
     else:
@@ -101,4 +146,8 @@ async def login(request: Request, username: str = Form(), password: str = Form()
 
 
 if __name__ == "__main__":
+    if sys.argv[1] == 'postgres':
+        db_type = DataBaseType.POSTGRES
+    elif sys.argv[1] == 'maria':
+        db_type = DataBaseType.MARIA
     uvicorn.run(app, host="127.0.0.1", port=8000)
