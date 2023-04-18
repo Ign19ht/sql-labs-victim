7f28e66c7c7b3efbb8ce2a02927474c97b31d9e8
ignat <petrov-ignat85@mail.ru>
2023-04-18 20:15:31 +0300

    fix main

--- backend/main.py
+++ backend/main.py
@@ -81,4 +81,4 @@ async def login(request: Request, username: str = Form(), password: str = Form()
 
 if __name__ == "__main__":
     fill_db()
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
