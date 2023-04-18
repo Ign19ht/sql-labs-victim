5b3cf64fd540d96c65fed883c3d998fb59ae9ba9
Ignat <petrov-ignat85@mail.ru>
2023-04-18 23:08:29 +0300

    fix vulnerabilities

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,21 @@
+import os
 from copy import copy
 
 import uvicorn
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import FileResponse, HTMLResponse
 import psycopg2
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from jinja2 import FileSystemLoader, Environment
 
 from db_gen import fill_db
 
 
-def db_request_postgres(query: str):
+def db_request_postgres(query: str, params: tuple):
     con = psycopg2.connect(database="db", user="postgres", host="db",
                            password="postgres", port="5432")
     cur = con.cursor()
-    cur.execute(query)
+    cur.execute(query, params)
     rows = cur.fetchall()
 
     cur.close()
@@ -41,27 +41,23 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates/victim")
 
-loader = FileSystemLoader(searchpath="templates/victim")
-unsafe_env = Environment(loader=loader, autoescape=False)
-
 
 @app.get("/filter", response_class=HTMLResponse)
 async def use_filter(request: Request, category: str):
-    query = f"SELECT title, body, image_name FROM news WHERE category='{category}' AND hidden=0"
+    query = "SELECT title, body, image_name FROM news WHERE category=%s AND hidden=0"
     try:
-        rows = db_request_postgres(query)
+        rows = db_request_postgres(query, (category,))
     except Exception:
         rows = []
-    response = unsafe_env.get_template('index.html').render(news_rows=get_news(rows), category=category)
-    # response = templates.TemplateResponse("index.html",
-    #                                       {"request": request, "news_rows": get_news(rows), "category": category})
+    response = templates.TemplateResponse("index.html",
+                                          {"request": request, "news_rows": get_news(rows), "category": category})
     return response
 
 
 @app.get("/", response_class=HTMLResponse)
 async def show_all(request: Request):
-    query = f"SELECT title, body, image_name FROM news WHERE hidden=0"
-    rows = db_request_postgres(query)
+    query = "SELECT title, body, image_name FROM news WHERE hidden=0"
+    rows = db_request_postgres(query, ())
     response = templates.TemplateResponse("index.html",
                                           {"request": request, "news_rows": get_news(rows), "category": "All"})
     return response
@@ -69,6 +65,8 @@ async def show_all(request: Request):
 
 @app.get("/image")
 async def get_image(image_name: str):
+    if ".." in image_name or not os.path.exists(f"Images/{image_name}"):
+        raise HTTPException(status_code=404, detail="Image not found")
     return FileResponse(f'Images/{image_name}')
 
 
@@ -80,8 +78,8 @@ async def open_login(request: Request):
 
 @app.post("/auth")
 async def login(request: Request, username: str = Form(), password: str = Form()):
-    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
-    rows = db_request_postgres(query)
+    query = "SELECT * FROM users WHERE username=%s AND password=%s"
+    rows = db_request_postgres(query, (username, password))
     if len(rows) > 0:
         return 'accepted'
     else:
